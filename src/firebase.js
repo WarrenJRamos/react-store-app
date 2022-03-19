4083efa7820a8a08322647b1222d5d0e36258aac
WarrenJRamos <wjnramos@gmail.com>
2022-03-19 08:53:20 -0600

    updated firebase to use most updated version

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,17 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+// Converted firebase@7.20.0 to the newest version firebase@9.6.9 (March 19, 8:52 am MT)
 
-// Your web app's Firebase configuration
+// "firebase/app" is a subpackage
+import { initializeApp } from "firebase/app";
+/*
+  * Import Firebase services we want to use
+  * They use the pattern of 'import {} from "firebase/<service>"'
+  * Import the service getter funtion inside the {}
+    E.g. { getAuth } for "firebase/auth"
+*/
+// "firebase/auth" monitors authentication state
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// Firebase configuration object
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,8 +21,18 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+// YOU MUST INITIALIZE YOUR FIREBASE APP FIRST BEFORE CALLING ANY SERVICE GETTER
+// iniitalizeApp creates a Firebase app that stores your Firebase configuation
+// for your project. It returns a Firebase app instance.
+// This instance is how the Firebase SDK knows how to connect to your
+// specific Firebase backend.
+const app = initializeApp(firebaseConfig);
+// Returns the Auth instance associated with the provided FirebaseApp. If
+// no instance exists, initializes an Auth instance with platform-specific
+// default dependencies.
+export const auth = getAuth(app);
 
-export const auth = app.auth();
+// export const auth = app.auth();
+
+// export the app just in case; I don't believe I use this anywhere
 export default app;
