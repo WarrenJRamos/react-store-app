c0cb17b11a4f0758f78c22b8b0b739592fc819d9
Dilshan <dilshan.mudali@gmail.com>
2022-03-19 02:31:51 -0400

    added routing, global theme, global context

---
+++ src/Styles/GlobalTheme.styled.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        //Main theme
+        colorOnyx: '#393e41',
+        colorTimberWolf: '#d3d0cb',
+        colorMaize: '#e2c044',
+        colorTeal: '#587B7F',
+        colorLavender: '#C2C2FF',
+        //experiment colors
+        colorFogra: "#040404",
+        colorCultured: '#f0f6f6'
+    }
+}
+
+export default theme;
