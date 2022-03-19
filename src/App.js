f6a0abff8f9eddb97191b579a56d08b1b2394673
WarrenJRamos <wjnramos@gmail.com>
2022-03-19 08:53:53 -0600

    Added some routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 // import {Routes, Route } from "react-router-dom";
 
-//global Context 
-import GlobalContext from './Context/globalContext';
+import { AuthProvider } from "./Context/AuthProvider";
 
-//styles 
-import {ThemeProvider} from 'styled-components'
-import GlobalStyles from './Styles/GlobalStyles.styled'
-import globalTheme from './Styles/GlobalTheme.styled'
+//global Context
+import GlobalContext from "./Context/globalContext";
+
+//styles
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./Styles/GlobalStyles.styled";
+import globalTheme from "./Styles/GlobalTheme.styled";
+import { Link, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import Signup from "./components/Authentication/Signup";
 
 function App() {
-
-  const [test, setTest] = useState(false)
+  const [test, setTest] = useState(false);
 
   return (
-    <GlobalContext.Provider value={{
-      test,
-      setTest,
-    }}>
+    <GlobalContext.Provider
+      value={{
+        test,
+        setTest,
+      }}
+    >
       <ThemeProvider theme={globalTheme}>
-        <GlobalStyles/>
-            {/* <Routes> */}
-            <>
-              <div>Hello world</div>
-            </>
-            {/* </Routes> */}
+        <GlobalStyles />
+        <AuthProvider>
+          <div>Welcome to our store!</div>
+          {/* Dummy Links */}
+          <Link to="/signup">Signup</Link>
+          <br />
+          <Link to="/products">Products</Link>
+          <br />
+          <Link to="/products/shoes">Products/shoes</Link>
+          <br />
+          <Link to="/products/mens">Products/mens</Link>
+          <br />
+          <Link to="/products/shoes">Products/womens</Link>
+          <br />
+
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Navigate to="/signup" />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/products/*"
+              element={
+                <div>
+                  Products List
+                  <Outlet />
+                </div>
+              }
+            >
+              <Route path="shoes" element={<div>Shoes Component</div>} />
+              <Route path="mens" element={<div>Mens Component</div>} />
+              <Route path="womens" element={<div>Womens Component</div>} />
+            </Route>
+          </Routes>
+        </AuthProvider>
       </ThemeProvider>
     </GlobalContext.Provider>
   );
