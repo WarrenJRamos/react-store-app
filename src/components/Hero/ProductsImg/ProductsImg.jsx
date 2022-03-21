82af0bb99c48f4dec4f3f590f92065927cef120d
WarrenJRamos <wjnramos@gmail.com>
2022-03-20 18:52:38 -0600

    messed around with the products header

--- src/components/Hero/ProductsImg/ProductsImg.jsx
+++ src/components/Hero/ProductsImg/ProductsImg.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import products1 from '../../../Images/Products/products1.jpg';
+import React from "react";
+import products1 from "../../../Images/Products/mens.jpg";
 
 const ProductsImg = (props) => {
   return (
     <div className={`${props.classes}`}>
-      <img src={products1} alt='new-items-img' className='img-container' />
+      <img src={products1} alt="new-items-img" className="img-container" />
+      <div className="overlay"></div>
     </div>
   );
 };
