31ebc919cda88254b5f093ed1ca67d489cec3969
WarrenJRamos <wjnramos@gmail.com>
2022-03-20 20:51:27 -0600

    started working on offcanvas for the cart items

---
+++ src/Styles/ShoppingCart/ShoppingCart.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ShoppingCartContainer = styled.div`
+  .shoppingCartButton {
+    border: none;
+    background-color: transparent;
+    color: ${(props) => props.colorCultured};
+  }
+`;
---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Button, Offcanvas } from "react-bootstrap";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { ShoppingCartContainer } from "../../Styles/ShoppingCart/ShoppingCart.styled";
+import { useTheme } from "styled-components";
+
+export default function ShoppingCart() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const theme = useTheme();
+
+  return (
+    <ShoppingCartContainer colorCultured={theme.colors.colorCultured}>
+      <button type="button" className="shoppingCartButton" onClick={handleShow}>
+        <ShoppingCartIcon />
+      </button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Cart</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>CART ITEMS WILL GO HERE</Offcanvas.Body>
+      </Offcanvas>
+    </ShoppingCartContainer>
+  );
+}
