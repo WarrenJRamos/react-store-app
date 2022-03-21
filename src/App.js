a7732e104036b20ec3d0fab65eaf18ab63fd7890
Dilshan <dilshan.mudali@gmail.com>
2022-03-21 00:47:27 -0400

    pagination is done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import NavigationMain from "./components/Navigation/NavigationMain";
-
-import { AuthProvider, useAuth } from "./Context/AuthProvider";
+import NavigationMain from './components/Navigation/NavigationMain';
+import axios from 'axios';
+import { AuthProvider, useAuth } from './Context/AuthProvider';
 
 //global Context
-import GlobalContext from "./Context/globalContext";
+import GlobalContext from './Context/globalContext';
 
 //styles
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./Styles/GlobalStyles.styled";
-import globalTheme from "./Styles/GlobalTheme.styled";
-import { Link, Navigate, Outlet, Route, Routes } from "react-router-dom";
-import Login from "./components/Authentication/Login";
-import ForgotPassword from "./components/Authentication/ForgotPassword";
-import UpdateProfile from "./components/Authentication/UpdateProfile";
-import Register from "./components/Authentication/Register";
-import ProtectedRoutes from "./ProtectedRoutes";
-import MyAccount from "./components/Account/MyAccount";
-import MenuListComposition from "./components/Account/MenuListComposition";
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './Styles/GlobalStyles.styled';
+import globalTheme from './Styles/GlobalTheme.styled';
+import { Link, Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import Login from './components/Authentication/Login';
+import ForgotPassword from './components/Authentication/ForgotPassword';
+import UpdateProfile from './components/Authentication/UpdateProfile';
+import Register from './components/Authentication/Register';
+import ProtectedRoutes from './ProtectedRoutes';
+import MyAccount from './components/Account/MyAccount';
+import MenuListComposition from './components/Account/MenuListComposition';
 
 //components
-import ProductList from "./components/Hero/ProductsList/ProductsList";
-import Products from "./components/Hero/Products";
-import Footer from "./components/Footer/Footer";
+import ProductList from './components/Hero/ProductsList/ProductsList';
+import Products from './components/Hero/Products';
+import Footer from './components/Footer/Footer';
 // import Signup from "./components/Authentication/Signup";
 // import Login from "./components/Authentication/Login";
 
-import ProductsComponent from "./Styles/Products/ProductsComponent.styled";
-import Landing from "./components/Landing/Landing";
-import LandingV2 from "./components/Landing/LandingV2";
+import ProductsComponent from './Styles/Products/ProductsComponent.styled';
+import LandingV2 from './components/Landing/LandingV2';
 function App() {
-  const [newProducts, setNewProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(6);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=15")
-      .then((res) => res.json())
-      .then((data) => setNewProducts(data));
+    const fetchProducts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://fakestoreapi.com/products?limit=15');
+      setAllProducts(res.data);
+      setLoading(false);
+    };
+
+    fetchProducts();
   }, []);
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentPageProducts = allProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
   return (
     <GlobalContext.Provider
       value={{
-        newProducts,
-        setNewProducts,
+        allProducts,
+        setAllProducts,
+        loading,
+        currentPageProducts,
+        productsPerPage,
+        setCurrentPage,
       }}
     >
       <ThemeProvider theme={globalTheme}>
@@ -52,46 +70,46 @@ function App() {
           <NavigationMain />
 
           <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<LandingV2 />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path='/' element={<Navigate to='/home' />} />
+            <Route path='/home' element={<LandingV2 />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
             <Route element={<ProtectedRoutes />}>
-              <Route path="/update-profile" element={<UpdateProfile />} />
-              <Route path="/my-account" element={<MyAccount />} />
+              <Route path='/update-profile' element={<UpdateProfile />} />
+              <Route path='/my-account' element={<MyAccount />} />
             </Route>
             <Route
-              path="/products/*"
+              path='/products/*'
               element={
-                <ProductsComponent className="products">
-                  <Products classes="products" />
+                <ProductsComponent className='products'>
+                  <Products classes='products' />
                   <Outlet />
                 </ProductsComponent>
               }
             >
               <Route
-                path="shoes"
+                path='shoes'
                 element={
-                  <ProductList classes="products__item products__item-list" />
+                  <ProductList classes='products__item products__item-list' />
                 }
               />
               <Route
-                path="mens"
+                path='mens'
                 element={
-                  <ProductList classes="products__item products__item-list" />
+                  <ProductList classes='products__item products__item-list' />
                 }
               />
               <Route
-                path="womens"
+                path='womens'
                 element={
-                  <ProductList classes="products__item products__item-list" />
+                  <ProductList classes='products__item products__item-list' />
                 }
               />
               <Route
-                path="new"
+                path='new'
                 element={
-                  <ProductList classes="products__item products__item-list" />
+                  <ProductList classes='products__item products__item-list' />
                 }
               />
             </Route>
--- src/components/Hero/ProductsList/ProductsList.jsx
+++ src/components/Hero/ProductsList/ProductsList.jsx
@@ -6,81 +6,43 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ProductCard from '../../../Styles/Products/ProductCard.styled';
 
+import PaginationComponent from '../../Pagination/PaginationComponent';
 const ProductsList = (props) => {
   const context = useContext(globalContext);
-  const newProducts = context.newProducts;
+  // const newProducts = context.newProducts;
+  const loading = context.loading;
+  const currentProducts = context.currentPageProducts;
 
-  // console.log(newProducts);
+  if (loading) {
+    return <h2>loading....</h2>;
+  }
 
   return (
     <div className={`${props.classes}`}>
-      {newProducts ? (
-        newProducts.map((product) => (
-          <Product key={product.id} product={product} />
-        ))
-      ) : (
-        <ProductCard className='card-container'>
-          <div className='img-container'>
-            <img src={test} alt='cloting-img' />
-          </div>
-          <div className='bottom'>
-            <div className='bottom-title'>
-              <span>Random item name</span>
-              <FavoriteBorderIcon className='fav' />
+      <div className={`${props.classes}`}>
+        {currentProducts ? (
+          currentProducts.map((product) => (
+            <Product key={product.id} product={product} />
+          ))
+        ) : (
+          <ProductCard className='card-container'>
+            <div className='img-container'>
+              <img src={test} alt='cloting-img' />
             </div>
-            <div className='bottom-price'>
-              <span>$55.45</span>
-              <AddShoppingCartIcon className='cart' />
+            <div className='bottom'>
+              <div className='bottom-title'>
+                <span>Random item name</span>
+                <FavoriteBorderIcon className='fav' />
+              </div>
+              <div className='bottom-price'>
+                <span>$55.45</span>
+                <AddShoppingCartIcon className='cart' />
+              </div>
             </div>
-          </div>
-        </ProductCard>
-      )}
-
-      {/* <ProductCard className='card-container'>
-        <div className='img-container'>
-          <img src={test} alt='cloting-img' />
-        </div>
-        <div className='bottom'>
-          <div className='bottom-title'>
-            <span>Random item name</span>
-            <FavoriteBorderIcon className='fav' />
-          </div>
-          <div className='bottom-price'>
-            <span>$55.45</span>
-            <AddShoppingCartIcon className='cart' />
-          </div>
-        </div>
-      </ProductCard>
-      <ProductCard className='card-container'>
-        <div className='img-container'>
-          <img src={test} alt='cloting-img' />
-        </div>
-        <div className='bottom'>
-          <div className='bottom-title'>
-            <span>Random item name</span>
-            <FavoriteBorderIcon className='fav' />
-          </div>
-          <div className='bottom-price'>
-            <span>$55.45</span>
-            <AddShoppingCartIcon className='cart' />
-          </div>
-        </div>
-      </ProductCard>
-      <ProductCard className='card-container'>
-        <div className='img-container'>
-          <img src={test} alt='cloting-img' />
-        </div>
-        <div className='bottom'>
-          <div className='bottom-title'>
-            <span>Random item name</span>
-            <FavoriteBorderIcon className='fav' />
-          </div>
-          <div className='bottom-price'>
-            <span>$55.45</span>
-            <AddShoppingCartIcon className='cart' />
-          </div>
-        </div>
-      </ProductCard> */}
+          </ProductCard>
+        )}
+      </div>
+      <PaginationComponent />
     </div>
   );
 };
