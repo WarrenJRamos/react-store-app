0a5bb22c1f9b2c58a9ca00c36a7a2eefab36d725
Dilshan <dilshan.mudali@gmail.com>
2022-04-04 18:00:02 -0400

    order page styling

--- src/components/Products/ProductsList/ProductsList.jsx
+++ src/components/Products/ProductsList/ProductsList.jsx
@@ -1,21 +1,21 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import Product from "./Product";
-import globalContext from "../../../Context/globalContext";
+import React, { useState, useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Product from './Product';
+import globalContext from '../../../Context/globalContext';
 
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 // import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import ProductCard from "../../../Styles/Products/ProductCard.styled";
+import ProductCard from '../../../Styles/Products/ProductCard.styled';
 
-import PaginationComponent from "../../Pagination/PaginationComponent";
-import { useAuth } from "../../../Context/AuthProvider";
+import PaginationComponent from '../../Pagination/PaginationComponent';
+import { useAuth } from '../../../Context/AuthProvider';
 const ProductsList = (props) => {
   const { currentUser } = useAuth();
   const context = useContext(globalContext);
   const loading = context.loading;
   const wishList = context.wishList;
-  console.log("Inside ProductsList: ", props);
-  console.log("Inside ProductsList: ", props.products);
+  console.log('Inside ProductsList: ', props);
+  console.log('Inside ProductsList: ', props.products);
   // need props.products
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(9);
@@ -78,16 +78,16 @@ const ProductsList = (props) => {
             );
           })
         ) : (
-          <ProductCard className="card-container">
-            <div className="img-container">
-              <img src={test} alt="cloting-img" />
+          <ProductCard className='card-container'>
+            <div className='img-container'>
+              <img src={test} alt='cloting-img' />
             </div>
-            <div className="bottom">
-              <div className="bottom-title">
+            <div className='bottom'>
+              <div className='bottom-title'>
                 <span>Random item name</span>
-                <FavoriteBorderIcon className="fav" />
+                <FavoriteBorderIcon className='fav' />
               </div>
-              <div className="bottom-price">
+              <div className='bottom-price'>
                 <span>$55.45</span>
               </div>
             </div>
